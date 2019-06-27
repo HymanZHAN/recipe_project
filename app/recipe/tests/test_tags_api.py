f237d415e1807b951c9bf71da4505c4ec9d1f9c0
Xucong Zhan <zhanxucong@gmail.com>
2019-06-27 17:35:40 -0400

    Added create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -62,3 +62,21 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0].get("name"), tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {"name": "Test tag"}
+        self.client.post(TAGS_URL, payload)
+
+        tag_exists = Tag.objects.filter(
+            user=self.user, name=payload.get("name")
+        ).exists()
+
+        self.assertTrue(tag_exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        invalid_payload = {"name": ""}
+        res: Response = self.client.post(TAGS_URL, invalid_payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,9 +1,5 @@
-# from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
@@ -11,7 +7,7 @@ from core.models import Tag
 from recipe.serializers import TagSerializer
 
 
-class TagViewSet(GenericViewSet, ListModelMixin):
+class TagViewSet(GenericViewSet, ListModelMixin, CreateModelMixin):
     """Manage tags in the database."""
 
     authentication_classes = (TokenAuthentication,)
@@ -22,3 +18,7 @@ class TagViewSet(GenericViewSet, ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
